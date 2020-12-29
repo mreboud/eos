8967984f7de45b7c6460f8a813dd16d6984080ff
Méril Reboud <meril.reboud@cern.ch>
2020-12-29 08:40:02 +0100

    [scattering] Add new directory for ee->ccbar scattering

---
+++ eos/scattering/ee-to-ccbar_TEST.cc
@@ -0,0 +1,11 @@
+#include <test/test.hh>
+#include <eos/utils/ee-to-ccbar.hh>
+#include <eos/utils/complex.hh>
+#include <iostream>
+#include <memory>
+
+#include <cmath>
+
+
+using namespace test;
+using namespace eos;
