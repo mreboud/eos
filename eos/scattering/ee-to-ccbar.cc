4246a44be9cfa1c9283647101aabdc44405ebf1a
Méril Reboud <meril.reboud@cern.ch>
2021-01-11 13:52:21 +0100

    [scattering] Implement K-matrix for ee->ccbar scattering

--- eos/scattering/ee-to-ccbar.cc
+++ eos/scattering/ee-to-ccbar.cc
@@ -1,2 +1,180 @@
-#include <eos/utils/kmatrix-impl.hh>
+/*
+ * Copyright (c) 2021 Méril Reboud
+ *
+ * This file is part of the EOS project. EOS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <eos/scattering/ee-to-ccbar.hh>
 #include <eos/utils/complex.hh>
+#include <eos/utils/kmatrix-impl.hh>
+
+#include <eos/utils/kinematic.hh>
+#include <eos/utils/options.hh>
+#include <eos/utils/private_implementation_pattern-impl.hh>
+
+#include <vector>
+#include <memory>
+
+namespace eos
+{
+
+    template <>
+    struct Implementation<EEToCCBar>
+    {
+//        std::shared_ptr<Model> model;
+
+        UsedParameter hbar;
+        UsedParameter alpha_em;
+        UsedParameter m_e;
+        UsedParameter m_D0;
+        UsedParameter m_D;
+
+        //Charmonium masses
+        UsedParameter m_psi2S;
+        UsedParameter m_psi3770;
+
+        // Channel-Resonance couplings
+        UsedParameter g0_psi2S_ee;
+        UsedParameter g0_psi3770_ee;
+        UsedParameter g0_psi2S_D0Dbar0;
+        UsedParameter g0_psi3770_D0Dbar0;
+        UsedParameter g0_psi2S_DpDm;
+        UsedParameter g0_psi3770_DpDm;
+
+
+        Implementation(const Parameters & p, const Options & o, ParameterUser & u) :
+//            model(Model::make(o.get("model", "SM"), p, o)),
+            hbar(p["hbar"], u),
+            alpha_em(p["QED::alpha_e(0)"], u),
+            m_e(p["mass::e"], u),
+            m_D0(p["mass::D^0"], u),
+            m_D(p["mass::D^+"], u),
+
+            m_psi2S(p["mass::psi(2S)"], u),
+            m_psi3770(p["mass::psi(3770)"], u),
+
+            g0_psi2S_ee(p["ee->ccbar::g0(psi(2S),ee)"], u),
+            g0_psi3770_ee(p["ee->ccbar::g0(psi(3770),ee)"], u),
+            g0_psi2S_D0Dbar0(p["ee->ccbar::g0(psi(2S),D^0Dbar^0)"], u),
+            g0_psi3770_D0Dbar0(p["ee->ccbar::g0(psi(3770),D^0Dbar^0)"], u),
+            g0_psi2S_DpDm(p["ee->ccbar::g0(psi(2S),D^+D^-)"], u),
+            g0_psi3770_DpDm(p["ee->ccbar::g0(psi(3770),D^+D^-)"], u)
+        {
+        }
+
+        const static unsigned nchannels = 3;
+        const static unsigned nresonances = 2;
+
+        inline double sigma_eetomumu(const double & s)
+        {
+
+            return 4.0 * M_PI * alpha_em*alpha_em / (3.0 * s);
+        }
+
+        inline double sigma_eetof(const double & s, const KMatrix<nchannels, nresonances> & K, const unsigned & channel)
+        {
+
+            // Conversion factor between GeV^2 and nb
+            const double speedoflight = 299792458.; //Exact value
+            const double GeVtonb = 10 * pow( 1.e18 * hbar * speedoflight, 2.0);
+
+            // Channel properties
+            const double Nf = K._channels[channel]->_N_orbital;
+            const double rhof = real(K._channels[channel]->rho(s));
+
+            // Get T-matrix[ee, channel]
+            const complex<double> T1f = K.tmatrix_row(0, s)[channel];
+
+            return GeVtonb * 16.*M_PI/s * Nf * rhof * norm(T1f);
+        }
+
+        inline double sigma_eetoccbar(const double & s, const KMatrix<nchannels, nresonances> K)
+        {
+
+            double total_xsec = 0.0;
+
+            for (int i = 0; i < nchannels; i++)
+            {
+                total_xsec += sigma_eetof(s, K, i);
+            }
+
+            return total_xsec;
+        }
+
+        double sigma_eetochannel(const double & E, const unsigned & channel)
+        {
+
+            // Build K Matrix
+            auto psi2S_res = std::make_shared<charmonium_resonance<nchannels, nresonances>>("psi2S_res", m_psi2S);
+            auto psi3770_res = std::make_shared<charmonium_resonance<nchannels, nresonances>>("psi3770_res", m_psi3770);
+
+            std::vector<Parameter> ee_g0s       {{g0_psi2S_ee, g0_psi3770_ee}};
+            std::vector<Parameter> D0Dbar0_g0s  {{g0_psi2S_D0Dbar0, g0_psi3770_D0Dbar0}};
+            std::vector<Parameter> DpDm_g0s     {{g0_psi2S_DpDm, g0_psi3770_DpDm}};
+
+            auto ee_chan        = std::make_shared<PPchan<nchannels, nresonances>>("ee_chan", m_e, m_e, 3, ee_g0s);
+            auto D0Dbar0_chan   = std::make_shared<PPchan<nchannels, nresonances>>("D0Dbar0_chan", m_D0, m_D0, 3, D0Dbar0_g0s);
+            auto DpDm_chan      = std::make_shared<PPchan<nchannels, nresonances>>("DpDm_chan", m_D, m_D, 3, DpDm_g0s);
+
+            KMatrix<nchannels, nresonances> K({ee_chan, D0Dbar0_chan, DpDm_chan}, {psi2S_res, psi3770_res}, "KMatrix");
+
+            return sigma_eetof(E*E, K, channel);
+        }
+
+        double Rc(const double & E)
+        {
+
+            // Build K Matrix
+            auto psi2S_res = std::make_shared<charmonium_resonance<nchannels, nresonances>>("psi2S_res", m_psi2S);
+            auto psi3770_res = std::make_shared<charmonium_resonance<nchannels, nresonances>>("psi3770_res", m_psi3770);
+
+            std::vector<Parameter> ee_g0s       {{g0_psi2S_ee, g0_psi3770_ee}};
+            std::vector<Parameter> D0Dbar0_g0s  {{g0_psi2S_D0Dbar0, g0_psi3770_D0Dbar0}};
+            std::vector<Parameter> DpDm_g0s     {{g0_psi2S_DpDm, g0_psi3770_DpDm}};
+
+            auto ee_chan        = std::make_shared<PPchan<nchannels, nresonances>>("ee_chan", m_e, m_e, 3, ee_g0s);
+            auto D0Dbar0_chan   = std::make_shared<PPchan<nchannels, nresonances>>("D0Dbar0_chan", m_D0, m_D0, 3, D0Dbar0_g0s);
+            auto DpDm_chan      = std::make_shared<PPchan<nchannels, nresonances>>("DpDm_chan", m_D, m_D, 3, DpDm_g0s);
+
+            KMatrix<nchannels, nresonances> K({ee_chan, D0Dbar0_chan, DpDm_chan}, {psi2S_res, psi3770_res}, "KMatrix");
+
+            return sigma_eetoccbar(E*E, K) / sigma_eetomumu(E*E);
+        }
+
+
+    };
+
+
+    EEToCCBar::EEToCCBar(const Parameters & parameters, const Options & options) :
+        PrivateImplementationPattern<EEToCCBar>(new Implementation<EEToCCBar>(parameters, options, *this))
+    {
+    }
+
+    EEToCCBar::~EEToCCBar()
+    {
+    }
+
+    double
+    EEToCCBar::sigma_eetoD0Dbar0(const double & E) const
+    {
+        return _imp->sigma_eetochannel(E, 1);
+    }
+
+    double
+    EEToCCBar::Rc(const double & E) const
+    {
+        return _imp->Rc(E);
+    }
+
+}
--- eos/scattering/ee-to-ccbar_TEST.cc
+++ eos/scattering/ee-to-ccbar_TEST.cc
@@ -1,11 +1,67 @@
 #include <test/test.hh>
-#include <eos/utils/ee-to-ccbar.hh>
+#include <eos/scattering/ee-to-ccbar.hh>
 #include <eos/utils/complex.hh>
-#include <iostream>
-#include <memory>
+#include <eos/utils/kmatrix-impl.hh>
 
-#include <cmath>
+#include <vector>
+#include <memory>
 
 
 using namespace test;
 using namespace eos;
+
+class eetoccbarTest :
+public TestCase
+{
+public:
+    eetoccbarTest() :
+    TestCase("ee->ccbar scattering tests")
+    {
+    }
+
+    virtual void run() const
+    {
+
+        constexpr double eps = 1e-5;
+
+        Parameters p = Parameters::Defaults();
+        p["ee->ccbar::g0(psi(2S),ee)"] = 0.1;
+        p["ee->ccbar::g0(psi(3770),ee)"] = 0.2;
+        p["ee->ccbar::g0(psi(2S),D^0Dbar^0)"] = 0.3;
+        p["ee->ccbar::g0(psi(3770),D^0Dbar^0)"] = 0.4;
+        p["ee->ccbar::g0(psi(2S),D^+D^-)"] = 0.5;
+        p["ee->ccbar::g0(psi(3770),D^+D^-)"] = 0.6;
+
+
+        // Build K Matrix
+        auto psi2S_res = std::make_shared<charmonium_resonance<3, 2>>("psi2S_res", p["mass::psi(2S)"]);
+        auto psi3770_res = std::make_shared<charmonium_resonance<3, 2>>("psi3770_res", p["mass::psi(3770)"]);
+
+        std::vector<Parameter> ee_g0s       {{p["ee->ccbar::g0(psi(2S),ee)"], p["ee->ccbar::g0(psi(3770),ee)"]}};
+        std::vector<Parameter> D0Dbar0_g0s  {{p["ee->ccbar::g0(psi(2S),D^0Dbar^0)"], p["ee->ccbar::g0(psi(3770),D^0Dbar^0)"]}};
+        std::vector<Parameter> DpDm_g0s     {{p["ee->ccbar::g0(psi(2S),D^+D^-)"], p["ee->ccbar::g0(psi(3770),D^+D^-)"]}};
+
+        auto ee_chan        = std::make_shared<PPchan<3, 2>>("ee_chan", p["mass::e"], p["mass::e"], 3, ee_g0s);
+        auto D0Dbar0_chan   = std::make_shared<PPchan<3, 2>>("D0Dbar0_chan", p["mass::D^0"], p["mass::D^0"], 3, D0Dbar0_g0s);
+        auto DpDm_chan      = std::make_shared<PPchan<3, 2>>("DpDm_chan", p["mass::D^+"], p["mass::D^+"], 3, DpDm_g0s);
+
+        KMatrix<3, 2> KMatrix32({ee_chan, D0Dbar0_chan, DpDm_chan}, {psi2S_res, psi3770_res}, "KMatrix");
+
+
+
+        auto KMatrix32s0 = KMatrix32.tmatrix_row(0, 9.0);
+        auto KMatrix32s1 = KMatrix32.tmatrix_row(0, 1.5);
+        auto KMatrix32s2 = KMatrix32.tmatrix_row(0, 0.001);
+
+
+        TEST_CHECK_NEARLY_EQUAL(KMatrix32s0[0].real(), 0.00870921,  eps);
+        TEST_CHECK_NEARLY_EQUAL(KMatrix32s0[0].imag(), 0.000075856, eps);
+        TEST_CHECK_NEARLY_EQUAL(KMatrix32s1[0].real(), 0.0037429,   eps);
+        TEST_CHECK_NEARLY_EQUAL(KMatrix32s1[0].imag(), 0.0000140095,eps);
+        TEST_CHECK_NEARLY_EQUAL(KMatrix32s2[0].real(), 0.00338731,  eps);
+        TEST_CHECK_NEARLY_EQUAL(KMatrix32s2[0].imag(), 0.000011468, eps);
+
+
+
+    }
+} eetoccbar_test;
